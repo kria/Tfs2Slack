46f2c32611219b335420739767b290790fb9a886
thomasDOTde <thomas@drewermann.org>
2016-04-03 21:10:18 +0200

    extended notification messages

--- TfsNotificationRelay.LyncPersistentChat/LyncPersistentChatNotifier.cs
+++ TfsNotificationRelay.LyncPersistentChat/LyncPersistentChatNotifier.cs
@@ -9,6 +9,7 @@ using DevCore.TfsNotificationRelay.Configuration;
 using DevCore.TfsNotificationRelay.Notifications;
 using Microsoft.TeamFoundation.Framework.Server;
 using TfsNotificationRelay.LyncPersistentChat;
+using Microsoft.TeamFoundation.Build.Server;
 
 namespace DevCore.TfsNotificationRelay.LyncPersistentChat
 {
@@ -19,15 +20,35 @@ namespace DevCore.TfsNotificationRelay.LyncPersistentChat
             string room = bot.GetSetting("room");
             string user = bot.GetSetting("user");
             string password = bot.GetSetting("password");
-            var lines = notification.ToMessage(bot, s => s);
-            string message = "";
-            if (lines != null)
-                foreach (string line in lines)
-                    message += String.Format("{0}\r\n", line);
             var client = new LyncPersistentChatClient();
             await client.ConnectAsync(user, password);
             await client.JoinRoomAsync(room);
-            await client.SendSimpleMessageAsync(message);
+            if (notification.GetType() == typeof(BuildNotification))
+            {
+                var typednotification = notification as BuildNotification;
+                var message = new LyncPersistentChatMessage(false,"Build Status Notification");
+                if (!typednotification.IsSuccessful)
+                {
+                    message.AppendEmoticon(LyncChatEmoticon.Crying);
+                    message.AppendText(String.Format("Build {0} Status {1}", typednotification.DisplayName,typednotification.BuildStatus));
+                    message.AppendText(String.Format("Shame on {0}", typednotification.UserName));
+                    message.AppendHyperLink("Details", new Uri(typednotification.BuildUrl));
+                } else
+                {
+                    message.AppendEmoticon(LyncChatEmoticon.Cool);
+                    message.AppendText(String.Format("Build {0} Status {1}", typednotification.DisplayName, typednotification.BuildStatus));
+                    message.AppendHyperLink("Details", new Uri(typednotification.BuildUrl));
+                }
+                await client.SendCustomMessageAsync(message);
+            }
+            else
+            {
+                var lines = notification.ToMessage(bot, s => s);
+                string message = "";
+                if (lines != null)
+                    message = String.Join("\r\n", lines);
+                await client.SendSimpleMessageAsync(message);
+            }
             client.Dispose();
         }
     }
